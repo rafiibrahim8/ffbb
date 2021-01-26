d11464a673114fc8f464d9395616881d2fad37b7
Ibrahim Rafi <rafiibrahim8@hotmail.com>
2021-01-26 12:14:32 +0600

    more bug fix

--- helper.py
+++ helper.py
@@ -1,7 +1,8 @@
 from re import findall
 from sys import argv
 
-REG_EXs = ['1[0-9]{9,}','id=([^/?&]+)','facebook\.com\/([^/?&]+)','[a-zA-Z0-9.]{5,}']
+# Got help form: https://github.com/circulosmeos/gdown.pl/blob/master/gdown.pl
+REG_EXs = ['1[0-9]{9,}','^id=([^/?&]+)','facebook\.com\/([^/?&]+)','[a-zA-Z0-9.]{5,}']
 
 for i in REG_EXs:
     x = findall(i,argv[1])
